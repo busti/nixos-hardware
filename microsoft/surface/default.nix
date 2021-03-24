790cd348ef590b292b8c92e016244fb5ffe90d84
moritz bust <mbust+github@mailbox.org>
2021-03-25 00:04:52 +0100

    Update default.nix

--- microsoft/surface/default.nix
+++ microsoft/surface/default.nix
@@ -2,7 +2,7 @@
   imports =
     [ ./kernel ./hardware_configuration.nix ./firmware/surface-go/ath10k ];
 
-  environment.systemPackages = with pkgs; [ surface-control ];
+  # environment.systemPackages = with pkgs; [ surface-control ];
   users.groups.surface-control = { };
   services.udev.packages = [ pkgs.surface-control ];
 }
